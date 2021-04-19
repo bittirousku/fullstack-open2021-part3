d9305bc4a2d76335ef58008160a548cc5f677880
Henrik Vesterinen <henrikv@iki.fi>
2021-04-19 15:38:59 +0300

    initial commit
    Signed-off-by: Henrik Vesterinen <henrikv@iki.fi>

---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require("express")
+const { v4: uuidv4 } = require("uuid")
+const morgan = require("morgan")
+const cors = require("cors")
+
+const app = express()
+
+morgan.token("body", (req, res) => JSON.stringify(req.body))
+
+app.use(express.json())
+// The morgan configuration was copy-pasted from my 2019 solution
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens.body(req, res),
+    ].join(" ")
+  })
+)
+app.use(cors())
+
+let people = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
+  },
+]
+
+app.get("/", (req, res) => {
+  res.redirect("/info")
+})
+
+app.get("/info", (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${people.length} people</p><p> ${new Date()}</p>`
+  )
+})
+
+app.get("/api/people", (req, res) => {
+  res.json(people)
+})
+
+app.get("/api/people/:id", (req, res) => {
+  let id = Number(req.params.id)
+  let person = people.find((person) => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.post("/api/people", (req, res) => {
+  let person = req.body
+  if (!person || !person.name || !person.number) {
+    return res.status(404).send({ error: "data missing" })
+  }
+  if (people.some((pers) => pers.name === person.name)) {
+    return res.status(404).send({ error: "name must be unique" })
+  }
+  person.id = uuidv4()
+  people.push(person)
+  console.log(person)
+  res.json(person)
+})
+
+app.delete("/api/people/:id", (req, res) => {
+  let id = Number(req.params.id)
+  people = people.filter((person) => person.id !== id)
+  res.status(204).end()
+  // TODO: better message for failed deletion
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
